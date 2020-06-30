b3002234323486d3741559e464eca22679e7b653
ANIKINKIRILL <ky.anikin@mail.ru>
2020-06-30 11:39:59 +0300

    Fix #47. Add owner param to FragmentState interface

--- adsmodule/src/main/java/tv/limehd/adsmodule/interfaces/FragmentState.kt
+++ adsmodule/src/main/java/tv/limehd/adsmodule/interfaces/FragmentState.kt
@@ -1,10 +1,11 @@
 package tv.limehd.adsmodule.interfaces
 
 import androidx.fragment.app.Fragment
+import tv.limehd.adsmodule.AdType
 
 interface FragmentState {
 
-    fun onSuccessState(fragment: Fragment)
-    fun onErrorState(message: String)
+    fun onSuccessState(fragment: Fragment, owner: AdType)
+    fun onErrorState(message: String, owner: AdType)
 
 }
